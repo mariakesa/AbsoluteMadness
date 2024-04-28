8363873178cb32a59527582e75464979204947a4
Maria <mariarosekesa@gmail.com>
2024-04-28 23:13:56 +0300

    playing around

---
+++ CrazySimulations/convenience_functions.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+def bin_spikes(spike_monitor, n_neurons, t_total, bin_size):
+    spikes_neurons = spike_monitor.i
+    spikes_times = spike_monitor.t*1000
+    time_series_array = []
+    bins = np.linspace(0, t_total, t_total//bin_size)
+    for neuron in range(n_neurons):
+        inds = np.where(spikes_neurons == neuron)
+        spike_times_neuron = spikes_times[inds[0]]
+        print(spike_times_neuron)
+        binned_ts = np.histogram(spike_times_neuron, bins)[0]
+        time_series_array.append(binned_ts)
+    time_series_array = np.array(time_series_array, dtype=object)
+    return time_series_array
